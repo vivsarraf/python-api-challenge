389de5b1a7a2604c305b3088be95e79ff5e26862
vivek sarraf <sarraf.vivek@gmail.com>
2024-02-12 18:03:04 -0500

    updated the logic for plots

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "4a52ecc67e6cf99086124d1f0c13e6a5"
+
+# Geoapify API Key
+geoapify_key = "737f1ea9fedc4d74b7867870efe7908bE737f1ea9fedc4d74b7867870efe7908b"
